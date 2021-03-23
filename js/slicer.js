399b4e78b590c6c9ef3061b8c2585347990f6f8a
NickoBelicko4 <locjmf@hotmail.com>
2021-03-22 18:19:26 -0600

    jquery

--- js/slicer.js
+++ js/slicer.js
@@ -1,12 +1,9 @@
-//almacenar slider en una variable
 var slider = $('#slider');
-//almacenar botones
 var siguiente = $('#btn-next');
 var anterior = $('#btn-prev');
 
-//mover ultima imagen al primer lugar
+
 $('#slider .slider__section:last').insertBefore('#slider .slider__section:first');
-//mostrar la primera imagen con un margen de -100%
 slider.css('margin-left', '-'+100+'%');
 
 function moverD() {
@@ -43,6 +40,5 @@ anterior.on('click',function() {
 	clearInterval(interval);
 	autoplay();
 });
-
 
 autoplay();
